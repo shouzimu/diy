4e6f37d6f6edf0da887b4fe63116dd2ef6eebc6a
shouzimu <intelcorecpu@gmail.com>
2019-08-07 07:30:29 +0800

    nacos demo

---
+++ course/nacos-demo/src/main/java/com/dh/nacos/ConfigController.java
@@ -0,0 +1,21 @@
+package com.dh.nacos;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("config")
+public class ConfigController {
+
+    @NacosValue(value = "${useLocalCache}", autoRefreshed = true)
+    private String useLocalCache;
+
+
+    @GetMapping(value = "/get")
+    public String get() {
+        return useLocalCache;
+    }
+
+}
---
+++ course/nacos-demo/src/main/java/com/dh/nacos/NacosConfigApplication.java
@@ -0,0 +1,14 @@
+package com.dh.nacos;
+
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@NacosPropertySource(dataId = "test01", autoRefreshed = true)
+public class NacosConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class, args);
+    }
+}
