45c3bb1416b18cd5974c74adefee7151d3a5081d
shouzimu <intelcorecpu@gmail.com>
2019-08-20 07:11:07 +0800

    启动参数

--- course/sentinel-dubbo/sentinel-provider/src/main/java/com/dh/course/sentinel/SentinelProviderApplication.java
+++ course/sentinel-dubbo/sentinel-provider/src/main/java/com/dh/course/sentinel/SentinelProviderApplication.java
@@ -9,6 +9,15 @@ import org.springframework.context.annotation.PropertySource;
 /**
  * 启动参数
  * -Dproject.name=sentinel-dubbo -Dcsp.sentinel.dashboard.server=127.0.0.1:8080 -Dcsp.sentinel.log.use.pid=true
+ *
+ * 启动多节点参数如下
+ *
+ * java -Dproject.name=sentinel-dubbo -Dcsp.sentinel.dashboard.server=127.0.0.1:8080 -Dcsp.sentinel.log.use.pid=true
+ * -Dserver.port=8081 -jar sentinel-provider-1.0-SNAPSHOT.jar
+ *
+ *
+ * java -Dproject.name=sentinel-dubbo -Dcsp.sentinel.dashboard.server=127.0.0.1:8080 -Dcsp.sentinel.log.use.pid=true
+ * -Dserver.port=8082 -jar sentinel-provider-1.0-SNAPSHOT.jar
  */
 @EnableDubbo(scanBasePackages = "com.dh.course.sentinel")
 @PropertySource(value = "classpath:/dubbo-config.properties")
